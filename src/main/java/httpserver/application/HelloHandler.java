37016c3abdfebb81674dfc846d2bbdb7804a16d0
No1Cheats <jan.imhof@gmail.com>
2020-03-09 09:50:42 +0100

    Added a minor Cookie

--- src/main/java/httpserver/application/HelloHandler.java
+++ src/main/java/httpserver/application/HelloHandler.java
@@ -9,11 +9,26 @@ import org.owasp.html.Sanitizers;
 public class HelloHandler implements RequestHandler {
     @Override
     public String handleRequest(HttpRequest request, HttpResponse response) {
+
+        String cookies = request.getHeader("Cookie");
+        String lang = "en";
+        if(cookies != null){
+            for(String cookie : cookies.split(";")){
+                String token[] = cookie.trim().split("=");
+                if(token[0].equals("lang")){
+                    lang = token[1];
+                }
+            }
+        }
+
+        response.addHeader("Set-Cookie", "lang=" + lang + "; Max-Age=30");
+
+
         if(request.isPost()){
             String username = request.getParameter("username");
             PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
             username = policy.sanitize(username);
-            String greeting = username == null ? "" : "Hi " + username;
+            String greeting = username == null ? "" : (lang.equals("en") ? "Hi " : "Hoi ") + username;
             response.addParameter("greeting", greeting);
             return "hello_post.html";
         }
