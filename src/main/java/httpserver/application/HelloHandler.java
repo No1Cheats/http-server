a2e4ee807bc2d86f343611bfb5850fcdfbe142cc
No1Cheats <jan.imhof@gmail.com>
2020-03-09 09:03:07 +0100

    Added HTML sanitation with OWASP to the HelloHandler too

--- src/main/java/httpserver/application/HelloHandler.java
+++ src/main/java/httpserver/application/HelloHandler.java
@@ -3,13 +3,16 @@ package httpserver.application;
 import httpserver.core.protocol.HttpRequest;
 import httpserver.core.protocol.HttpResponse;
 import httpserver.framework.RequestHandler;
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 
 public class HelloHandler implements RequestHandler {
     @Override
     public String handleRequest(HttpRequest request, HttpResponse response) {
         if(request.isPost()){
             String username = request.getParameter("username");
-            username = username.replaceAll("<", "!");
+            PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+            username = policy.sanitize(username);
             String greeting = username == null ? "" : "Hi " + username;
             response.addParameter("greeting", greeting);
             return "hello_post.html";
